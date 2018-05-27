512e3c211a5bd205aab1218ea9219b25df24e3d6
asm32 <asm32@qq.com>
2018-05-27 10:49:07 +0800

    php-cocktailsort-demo-1.php ... modify

--- php-Arithmetic-2018/php-cocktailsort-demo-1.php/php-cocktailsort-demo-1.php
+++ php-Arithmetic-2018/php-cocktailsort-demo-1.php/php-cocktailsort-demo-1.php
@@ -1,34 +1,34 @@
 <?php
 // php-cocktailsort-demo-1.php
 class CocktailSortDemo1{
-    function DisplayData($data){
-        print join($data, ', ')."\n";
-    }
+	function DisplayData($data){
+		print join($data, ', ')."\n";
+	}
 
-    function Swap(&$data, $i, $j){
-        $temp = $data[$i];
-        $data[$i] = $data[$j];
-        $data[$j] = $temp;
-    }
+	function Swap(&$data, $i, $j){
+		$temp = $data[$i];
+		$data[$i] = $data[$j];
+		$data[$j] = $temp;
+	}
 
-    function CocktailSort(&$data){
-        $nLeft = 0;
-        $nRight = sizeof($data) - 1;
-        while($nLeft < $nRight){
-            for($i = $nLeft; $i < $nRight; $i++){
-                if($data[$i] > $data[$i + 1]){
-                    $this->Swap($data, $i, $i + 1);
-                }
-            }
-            $nRight--;
-            for($i = $nRight; $i > $nLeft; $i--){
-                if($data[$i - 1] > $data[$i]){
-                    $this->Swap($data, $i - 1, $i);
-                }
-            }
-            $nLeft++;
-        }
-    }
+	function CocktailSort(&$data){
+		$nLeft = 0;
+		$nRight = sizeof($data) - 1;
+		while($nLeft < $nRight){
+			for($i = $nLeft; $i < $nRight; $i++){
+				if($data[$i] > $data[$i + 1]){
+					$this->Swap($data, $i, $i + 1);
+				}
+			}
+			$nRight--;
+			for($i = $nRight; $i > $nLeft; $i--){
+				if($data[$i - 1] > $data[$i]){
+					$this->Swap($data, $i - 1, $i);
+				}
+			}
+			$nLeft++;
+		}
+	}
 }
 
 // $data = array(41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36);
