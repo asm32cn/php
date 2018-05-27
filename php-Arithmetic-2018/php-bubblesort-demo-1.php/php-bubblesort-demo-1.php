c23090f241c91859725c1ef58cd1b8158c77ba87
asm32 <asm32@qq.com>
2018-05-27 10:50:07 +0800

    php-bubblesort-demo-1.php ... modify

--- php-Arithmetic-2018/php-bubblesort-demo-1.php/php-bubblesort-demo-1.php
+++ php-Arithmetic-2018/php-bubblesort-demo-1.php/php-bubblesort-demo-1.php
@@ -1,22 +1,22 @@
 <?php
 // php-bubblesort-demo-1.php
 class BubbleSortDemo1{
-    function DisplayData($data){
-        print join($data, ', ')."\n";
-    }
+	function DisplayData($data){
+		print join($data, ', ')."\n";
+	}
 
-    function BubbleSort(&$data){
-        $n = sizeof($data);
-        for($j = 0; $j < $n - 1; $j++){
-            for($i = 0; $i < $n - 1 - $j; $i++){
-                if($data[$i] > $data[$i + 1]){
-                    $temp = $data[$i];
-                    $data[$i] = $data[$i + 1];
-                    $data[$i + 1] = $temp;
-                }
-            }
-        }
-    }
+	function BubbleSort(&$data){
+		$n = sizeof($data);
+		for($j = 0; $j < $n - 1; $j++){
+			for($i = 0; $i < $n - 1 - $j; $i++){
+				if($data[$i] > $data[$i + 1]){
+					$temp = $data[$i];
+					$data[$i] = $data[$i + 1];
+					$data[$i + 1] = $temp;
+				}
+			}
+		}
+	}
 }
 
 // $data = array(41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36);
