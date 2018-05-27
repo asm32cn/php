71726a556f14765e66a404ff8146d3840ec1c149
asm32 <asm32@qq.com>
2018-05-27 10:45:56 +0800

    php-insertion-sort-dichotomy-demo-1.php ... modify

--- php-Arithmetic-2018/php-insertion-sort-dichotomy-demo-1.php/php-insertion-sort-dichotomy-demo-1.php
+++ php-Arithmetic-2018/php-insertion-sort-dichotomy-demo-1.php/php-insertion-sort-dichotomy-demo-1.php
@@ -2,30 +2,30 @@
 // php-insertion-sort-dichotomy-demo-1.php
 
 class InsertionSortDichotomyDemo1{
-    function DisplayData($data){
-        print join($data, ', ')."\n";
-    }
+	function DisplayData($data){
+		print join($data, ', ')."\n";
+	}
 
-    function InsertionSortDichotomy(&$data){
-        $n = sizeof($data);
-        for($i = 1; $i < $n; $i++){
-            $nGet = $data[$i];
-            $nLeft = 0;
-            $nRight = $i - 1;
-            while($nLeft <= $nRight){
-                $nMid = (int)(($nLeft + $nRight) / 2);
-                if($data[$nMid] > $nGet){
-                    $nRight = $nMid - 1;
-                }else{
-                    $nLeft = $nMid + 1;
-                }
-            }
-            for($j = $i - 1; $j >= $nLeft; $j--){
-                $data[$j + 1] = $data[$j];
-            }
-            $data[$nLeft] = $nGet;
-        }
-    }
+	function InsertionSortDichotomy(&$data){
+		$n = sizeof($data);
+		for($i = 1; $i < $n; $i++){
+			$nGet = $data[$i];
+			$nLeft = 0;
+			$nRight = $i - 1;
+			while($nLeft <= $nRight){
+				$nMid = (int)(($nLeft + $nRight) / 2);
+				if($data[$nMid] > $nGet){
+					$nRight = $nMid - 1;
+				}else{
+					$nLeft = $nMid + 1;
+				}
+			}
+			for($j = $i - 1; $j >= $nLeft; $j--){
+				$data[$j + 1] = $data[$j];
+			}
+			$data[$nLeft] = $nGet;
+		}
+	}
 }
 
 $data = array(41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36);
