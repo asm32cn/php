85e3cd360b8d39ba64ea2f0f104f78cd9d1447bf
asm32 <asm32@qq.com>
2018-05-27 10:48:17 +0800

    php-counting-sort-demo-1.php ... modify

--- php-Arithmetic-2018/php-counting-sort-demo-1.php/php-counting-sort-demo-1.php
+++ php-Arithmetic-2018/php-counting-sort-demo-1.php/php-counting-sort-demo-1.php
@@ -4,30 +4,30 @@
 define('k', 100);
 
 class CountingSortDemo1{
-    private $C = array(k);
-    function DisplayData($data){
-        print join($data, ', ')."\n";
-    }
+	private $C = array(k);
+	function DisplayData($data){
+		print join($data, ', ')."\n";
+	}
 
-    function CountingSort(&$data){
-        $n = sizeof($data);
-        for($i = 0; $i < k; $i++){
-            $this->C[$i] = 0;
-        }
-        for($i = 0; $i < $n; $i++){
-            $this->C[$data[$i]]++;
-        }
-        for($i = 1; $i < k; $i++){
-            $this->C[$i] += $this->C[$i - 1];
-        }
-        $B = array($n);
-        for($i = $n - 1; $i >= 0; $i--){
-            $B[--$this->C[$data[$i]]] = $data[$i];
-        }
-        for($i = 0; $i < $n; $i++){
-            $data[$i] = $B[$i];
-        }
-    }
+	function CountingSort(&$data){
+		$n = sizeof($data);
+		for($i = 0; $i < k; $i++){
+			$this->C[$i] = 0;
+		}
+		for($i = 0; $i < $n; $i++){
+			$this->C[$data[$i]]++;
+		}
+		for($i = 1; $i < k; $i++){
+			$this->C[$i] += $this->C[$i - 1];
+		}
+		$B = array($n);
+		for($i = $n - 1; $i >= 0; $i--){
+			$B[--$this->C[$data[$i]]] = $data[$i];
+		}
+		for($i = 0; $i < $n; $i++){
+			$data[$i] = $B[$i];
+		}
+	}
 }
 
 // $data = array(41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36);
