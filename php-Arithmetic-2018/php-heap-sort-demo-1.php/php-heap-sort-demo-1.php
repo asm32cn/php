0f819958b41da6e5fca37f12ff13569eb46285c0
asm32 <asm32@qq.com>
2018-05-27 10:47:28 +0800

    php-heap-sort-demo-1.php ... modify

--- php-Arithmetic-2018/php-heap-sort-demo-1.php/php-heap-sort-demo-1.php
+++ php-Arithmetic-2018/php-heap-sort-demo-1.php/php-heap-sort-demo-1.php
@@ -2,44 +2,44 @@
 // php-heap-sort-demo-1.php
 
 class HeapSortDemo1{
-    function DisplayData($data){
-        print join($data, ', ')."\n";
-    }
+	function DisplayData($data){
+		print join($data, ', ')."\n";
+	}
 
-    function HeapSort(&$data){
-        $nHeapSize = sizeof($data);
-        // BuildHeap
-        for($i = (int)($nHeapSize / 2) - 1; $i >= 0; $i--){
-            $this->Heapify($data, $i, $nHeapSize);
-        }
-        // HeapSort
-        while($nHeapSize > 1){
-            $this->Swap($data, 0, --$nHeapSize);
-            $this->Heapify($data, 0, $nHeapSize);
-        }
-    }
+	function HeapSort(&$data){
+		$nHeapSize = sizeof($data);
+		// BuildHeap
+		for($i = (int)($nHeapSize / 2) - 1; $i >= 0; $i--){
+			$this->Heapify($data, $i, $nHeapSize);
+		}
+		// HeapSort
+		while($nHeapSize > 1){
+			$this->Swap($data, 0, --$nHeapSize);
+			$this->Heapify($data, 0, $nHeapSize);
+		}
+	}
 
-    function Heapify(&$data, $i, $nSize){
-        $nLeftChild = 2 * $i + 1;
-        $nRightChild = 2 * $i + 2;
-        $nMax = $i;
-        if($nLeftChild < $nSize && $data[$nLeftChild] > $data[$nMax]){
-            $nMax = $nLeftChild;
-        }
-        if($nRightChild < $nSize && $data[$nRightChild] > $data[$nMax]){
-            $nMax = $nRightChild;
-        }
-        if($nMax != $i){
-            $this->Swap($data, $i, $nMax);
-            $this->Heapify($data, $nMax, $nSize);
-        }
-    }
+	function Heapify(&$data, $i, $nSize){
+		$nLeftChild = 2 * $i + 1;
+		$nRightChild = 2 * $i + 2;
+		$nMax = $i;
+		if($nLeftChild < $nSize && $data[$nLeftChild] > $data[$nMax]){
+			$nMax = $nLeftChild;
+		}
+		if($nRightChild < $nSize && $data[$nRightChild] > $data[$nMax]){
+			$nMax = $nRightChild;
+		}
+		if($nMax != $i){
+			$this->Swap($data, $i, $nMax);
+			$this->Heapify($data, $nMax, $nSize);
+		}
+	}
 
-    function Swap(&$data, $i, $j){
-        $temp = $data[$i];
-        $data[$i] = $data[$j];
-        $data[$j] = $temp;
-    }
+	function Swap(&$data, $i, $j){
+		$temp = $data[$i];
+		$data[$i] = $data[$j];
+		$data[$j] = $temp;
+	}
 }
 
 // $data = array(41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36);
